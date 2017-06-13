42754ca159a805ef6b42619221e8cb17fd15b3e0
Moon Antonio <pinchodev91@gmail.com>
2017-06-13 13:13:47 +0200

    Add MenuCircular.cs

---
+++ Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/MenuCircular.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MenuCircular.cs (13/06/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control del Menu Circular									\\
+// Fecha Mod:		13/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.UI
+{
+	/// <summary>
+	/// <para>Control del Menu Circular</para>
+	/// </summary>
+	[AddComponentMenu("Moon Antonio/UI/MenuCircular")]
+	public class MenuCircular : MonoBehaviour 
+	{
+
+	}
+}
