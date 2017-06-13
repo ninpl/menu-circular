07e76f0a08f64677942d54012f3f05d45d86d7ed
Moon Antonio <pinchodev91@gmail.com>
2017-06-13 13:49:07 +0200

    Funcionalidad de Accion.cs

--- Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Clases/Accion.cs
+++ Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Clases/Accion.cs
@@ -20,9 +20,46 @@ namespace MoonAntonio.UI
 	[AddComponentMenu("Moon Antonio/UI/Clases/Accion")]
 	public class Accion
 	{
-		#region Propiedades
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Color de la accion</para>
+		/// </summary>
+		private Color color;										// Color de la accion
+		/// <summary>
+		/// <para>Sprite de la accion</para>
+		/// </summary>
+		private Sprite sprite;										// Sprite de la accion
+		/// <summary>
+		/// <para>Titulo de la accion</para>
+		/// </summary>
+		private string titulo;										// Titulo de la accion
+		#endregion
 
-		
+		#region Propiedades
+		/// <summary>
+		/// <para>Color de la accion.</para>
+		/// </summary>
+		public Color Color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+		/// <summary>
+		/// <para>Sprite de la accion</para>
+		/// </summary>
+		public Sprite Sprite
+		{
+			get { return sprite; }
+			set { sprite = value; }
+		}
+		/// <summary>
+		/// <para>Titulo de la accion</para>
+		/// </summary>
+		public string Titulo
+		{
+			get { return titulo; }
+			set { titulo = value; }
+		}
 		#endregion
 	}
 }
