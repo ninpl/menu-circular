fb414959352713e2c3d2e0121a30fb801fb62f66
Moon Antonio <pinchodev91@gmail.com>
2017-06-13 13:25:19 +0200

    Add manager

---
+++ Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Interactivo.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Interactivo.cs (13/06/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control de la interactividad del objeto						\\
+// Fecha Mod:		13/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Control de la interactividad del objeto</para>
+	/// </summary>
+	[AddComponentMenu("Moon Antonio/UI/Interactivo")]
+	public class Interactivo : MonoBehaviour 
+	{
+
+	}
+}
---
+++ Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/ManagerMenuCircular.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ManagerMenuCircular.cs (13/06/2017)											\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Manager del menu circular									\\
+// Fecha Mod:		13/06/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Manager del menu circular	</para>
+	/// </summary>
+	[AddComponentMenu("Moon Antonio/UI/ManagerMenuCircular")]
+	public class ManagerMenuCircular : MonoBehaviour 
+	{
+
+	}
+}
