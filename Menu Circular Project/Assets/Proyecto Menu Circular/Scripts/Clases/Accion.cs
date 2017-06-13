33bf8ae60cfd15088bd1a20c2e027cb27d2e9437
Moon Antonio <pinchodev91@gmail.com>
2017-06-13 14:09:06 +0200

    Serializado de Accion.cs

--- Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Clases/Accion.cs
+++ Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Clases/Accion.cs
@@ -21,46 +21,19 @@ namespace MoonAntonio.UI
 	[Serializable]
 	public class Accion
 	{
-		#region Variables Privadas
+		#region Variables Publicas
 		/// <summary>
 		/// <para>Color de la accion</para>
 		/// </summary>
-		private Color color;										// Color de la accion
+		public Color color;											// Color de la accion
 		/// <summary>
 		/// <para>Sprite de la accion</para>
 		/// </summary>
-		private Sprite sprite;										// Sprite de la accion
+		public Sprite sprite;										// Sprite de la accion
 		/// <summary>
 		/// <para>Titulo de la accion</para>
 		/// </summary>
-		private string titulo;										// Titulo de la accion
-		#endregion
-
-		#region Propiedades
-		/// <summary>
-		/// <para>Color de la accion.</para>
-		/// </summary>
-		public Color Color
-		{
-			get { return color; }
-			set { color = value; }
-		}
-		/// <summary>
-		/// <para>Sprite de la accion</para>
-		/// </summary>
-		public Sprite Sprite
-		{
-			get { return sprite; }
-			set { sprite = value; }
-		}
-		/// <summary>
-		/// <para>Titulo de la accion</para>
-		/// </summary>
-		public string Titulo
-		{
-			get { return titulo; }
-			set { titulo = value; }
-		}
+		public string titulo;										// Titulo de la accion
 		#endregion
 	}
 }
