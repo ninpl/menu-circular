647cad7ac3d7cbaba9efa04f39d3fe7dc79a3260
Moon Antonio <pinchodev91@gmail.com>
2017-06-13 14:00:28 +0200

    Funcionalidad menu circular

--- Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Comun/MenuCircular.cs
+++ Menu Circular Project/Assets/Proyecto Menu Circular/Scripts/Comun/MenuCircular.cs
@@ -19,6 +19,30 @@ namespace MoonAntonio.UI
 	[AddComponentMenu("Moon Antonio/UI/MenuCircular")]
 	public class MenuCircular : MonoBehaviour 
 	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Prefab del boton del menu circular.</para>
+		/// </summary>
+		public BtnMenuCircular btnPrefab;						// Prefab del boton del menu circular
+		/// <summary>
+		/// <para>Boton seleccionado actualmente.</para>
+		/// </summary>
+		public BtnMenuCircular seleccionado;                    // Boton seleccionado actualmente
+		#endregion
 
+		#region Inicializadores
+		/// <summary>
+		/// <para>Inicializador de <see cref="MenuCircular"/>.</para>
+		/// </summary>
+		private void Start()// Inicializador de MenuCircular
+		{
+			// Instanciar el boton
+			BtnMenuCircular newBtn = Instantiate(btnPrefab) as BtnMenuCircular;
+
+			// Fijar la posicion y el padre del boton
+			newBtn.transform.SetParent(transform, false);
+			newBtn.transform.localPosition = new Vector3(0.0f, 100.0f, 0.0f);
+		}
+		#endregion
 	}
 }
